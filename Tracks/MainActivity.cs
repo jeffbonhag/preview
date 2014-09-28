b55f3143a4c6da9b7ae18eb3c31450a9c8b18f18
Jeff Bonhag <jeff@thebonhags.com>
2014-09-28 13:11:06 -0400

    Roxanne!

--- Tracks/MainActivity.cs
+++ Tracks/MainActivity.cs
@@ -9,17 +9,20 @@ using Android.OS;
 using System.Net;
 using System.IO;
 using System.Runtime.Serialization.Json;
+using Android.Media;
 
 namespace Tracks
 {
 	[Activity (Label = "Tracks", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		MediaPlayer _player;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+
+			_player = new MediaPlayer();
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
@@ -29,12 +32,16 @@ namespace Tracks
 			Button button = FindViewById<Button> (Resource.Id.myButton);
 			
 			button.Click += delegate {
-				var request = HttpWebRequest.Create("http://itunes.apple.com/search?term=beyonce%20diva");
+				var request = HttpWebRequest.Create("http://itunes.apple.com/search?term=roxanne");
 				request.Method = "GET";
 
 				using (HttpWebResponse response = request.GetResponse() as HttpWebResponse) {
 					DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Response));
 					Response searchResults = (Response)jsonSerializer.ReadObject(response.GetResponseStream());
+					var url = searchResults.Results[0].PreviewUrl;
+					_player.SetDataSource(url);
+					_player.Prepare();
+					_player.Start();
 					button.Text = searchResults.Results[0].ArtistName;
 				}
 			};
